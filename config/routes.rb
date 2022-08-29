5eca1d535e81f8614f8d5a3c5060e37f9f0a4697
Nilton <niltonroese@gmail.com>
2022-08-29 22:08:01 +1000

    all passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  # get 'songs/create'
+
+  # get 'songs/show'
+
+  # get 'songs/update'
+
+  # get 'songs/index'
+
+  # get 'genres/create'
+
+  # get 'genres/update'
+
+  # get 'genres/show'
+
+  # get 'artist/create'
+
+  # get 'artist/update'
+
+  # get 'artist/show'
+
+  # get 'artist/new'
+
+  resources :artists, :genres, :songs, only: [:index, :create, :new, :edit, :show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
